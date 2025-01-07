1af33aec3c9a120d9e2e9ebd3f94641e79a2d50b
frankbits <62514408+frankbits@users.noreply.github.com>
2025-01-07 19:41:03 +0100

    added flie.py + tha_framework.launch.py

--- src/roboss/setup.py
+++ src/roboss/setup.py
@@ -2,15 +2,21 @@ from setuptools import find_packages, setup
 
 package_name = 'roboss'
 
+data_files = []
+data_files.append(
+    ("share/ament_index/resource_index/packages", ["resource/" + package_name])
+)
+data_files.append(("share/" + package_name, ["package.xml"]))
+
+data_files.append(
+    ("share/" + package_name + "/launch", ["launch/tha_framework.launch.py"])
+)
+
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='rosrunner',
@@ -19,7 +25,6 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': ["roboss = roboss.flie:main"],
     },
 )
