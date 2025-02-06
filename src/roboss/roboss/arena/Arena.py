a453b07b13c5951c7dcfa9e7380a0164a6d34447
moeux <68925026+moeux@users.noreply.github.com>
2025-02-06 18:32:30 +0100

    Replaced nested loop with list comprehension and removed unnecessary code.

--- src/roboss/roboss/arena/Arena.py
+++ src/roboss/roboss/arena/Arena.py
@@ -24,25 +24,15 @@ class Obstacle(Segment):
 class Arena:
     size: int
     segments: List[List[Segment]] = []
-    robots: List
 
     def __init__(self, src):
-        # get arena-data from JSON-file
         parsed_data = json.load(open(src))
-        # Map JSON data
         self.size = parsed_data[Config.Arena.ARENA_KEY][Config.Arena.SIZE_KEY]
-
-        for row in parsed_data[Config.Arena.ARENA_KEY][Config.Arena.SEGMENTS_KEY]:
-            row_segments = []
-
-            for segment in row:
-                if segment[Config.Arena.OBSTACLE_KEY]:
-                    segment = Obstacle()
-                else:
-                    segment = Field()
-                row_segments.append(segment)
-
-            self.segments.append(row_segments)
+        self.segments = [
+            [
+                Obstacle() if segment[Config.Arena.OBSTACLE_KEY] else Field() for segment in row
+            ] for row in parsed_data[Config.Arena.ARENA_KEY][Config.Arena.SEGMENTS_KEY]
+        ]
 
     def get_mapped_arena(self):
         return list(
