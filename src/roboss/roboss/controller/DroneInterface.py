60c990d471b0f9c85fd6a689452ec95d85743a60
moeux <68925026+moeux@users.noreply.github.com>
2025-02-06 18:40:44 +0100

    Added type hints and doc strings.

--- src/roboss/roboss/controller/DroneInterface.py
+++ src/roboss/roboss/controller/DroneInterface.py
@@ -3,25 +3,48 @@ from abc import ABC, abstractmethod
 
 class DroneInterface(ABC):
     @abstractmethod
-    def takeoff(self):
+    def takeoff(self) -> None:
+        """
+        Issues a take-off command to the drone.
+        """
         pass
 
     @abstractmethod
-    def land(self):
+    def land(self) -> None:
+        """
+        Issues a landing command to the drone.
+        """
         pass
 
     @abstractmethod
-    def send_target(self, position) -> None:
+    def send_target(self, position: tuple[float, float, float]) -> None:
+        """
+        Sends a target position to the drone.
+        :param position: The target position (x, y, z).
+        :return:
+        """
         pass
 
     @abstractmethod
     def get_range(self) -> float:
+        """
+        Returns the range sensor value.
+        :return: The height.
+        """
         pass
 
     @abstractmethod
     def get_position(self) -> tuple[float, float, float]:
+        """
+        Returns the position of the drone.
+        :return: The current position (x, y, z).
+        """
         pass
 
     @abstractmethod
     def get_time(self) -> float:
+        """
+        Returns the current timestamp in seconds.
+        :return: Seconds.
+        """
         pass
