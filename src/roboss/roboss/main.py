2cbb822c1093c2850fd969d577f19e994b95f0ad
moeux <68925026+moeux@users.noreply.github.com>
2025-02-06 18:50:14 +0100

    Adapted main.py to changes.

--- src/roboss/roboss/main.py
+++ src/roboss/roboss/main.py
@@ -1,6 +1,7 @@
 from arena.Arena import Arena
 from config import Config
 from controller.DroneController import DroneController
+from controller.WebotsDroneInterface import WebotsDroneInterface
 from render.Renderer import draw_board
 from route.RoutePlanner import RoutePlanner
 
@@ -10,8 +11,8 @@ if __name__ == "__main__":
     arena = Arena(Config.Arena.PATH)
     draw_board(arena.get_mapped_arena())
 
-    drone = DroneController()
-    start_pos = drone.get_position()
+    drone = DroneController(WebotsDroneInterface())
+    start_pos = drone.controller.get_position()
 
     route = RoutePlanner(arena.segments, start_pos).find_route()
     print('route', route)
@@ -19,10 +20,10 @@ if __name__ == "__main__":
     print("Takeoff")
     drone.controller.takeoff()
     print("Sending target (1,1,1)")
-    drone.move_to([1.0, 1.0, 1.0])
+    drone.move_to((1.0, 1.0, 1.0))
     print("Getting range and position")
-    print(drone.controller.getRange(), drone.controller.get_position())
+    print(drone.controller.get_range(), drone.controller.get_position())
     print("Sending target (2,2,2)")
-    drone.move_to([2, 2, 2])
+    drone.move_to((2, 2, 2))
     print("Landing")
     drone.controller.land()
