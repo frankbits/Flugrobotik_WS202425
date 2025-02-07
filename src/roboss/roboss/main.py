992d604f5afa3f76576fa31898cf3a89b46bb86c
moeux <68925026+moeux@users.noreply.github.com>
2025-02-07 21:53:36 +0100

    Added rclpy init and spin code and sleep statements.

--- src/roboss/roboss/main.py
+++ src/roboss/roboss/main.py
@@ -1,7 +1,8 @@
+import rclpy
 from .arena.Arena import Arena
 from .config import Config
 from .controller.DroneController import DroneController
-from .controller.WebotsDroneInterface import WebotsDroneInterface
+from .controller.ROSDroneInterface import ROSDroneInterface
 from .render.Renderer import draw_board
 from .route.RoutePlanner import RoutePlanner
 
@@ -10,23 +11,35 @@ def main():
 
     arena = Arena(Config.Arena.PATH)
     draw_board(arena.get_mapped_arena())
-
-    drone = DroneController(WebotsDroneInterface())
-    start_pos = drone.controller.get_position()
-
-    route = RoutePlanner(arena.segments, start_pos).find_route()
-    print('route', route)
+    
+    rclpy.init()
+    
+    interface = ROSDroneInterface()
+    drone = DroneController(interface)
 
     print("Takeoff")
+    drone.controller.sleep(4)
     drone.controller.takeoff()
+    drone.controller.sleep(4)
     print("Sending target (1,1,1)")
     drone.move_to([1.0, 1.0, 1.0])
+    drone.controller.sleep(2)
     print("Getting range and position")
     print(drone.controller.get_range(), drone.controller.get_position())
     print("Sending target (2,2,2)")
-    drone.move_to([2, 2, 2])
+    drone.move_to([2.0, 2.0, 2.0])
+    drone.controller.sleep(2)
     print("Landing")
     drone.controller.land()
+    drone.controller.sleep(4)
+    
+    try:
+        while rclpy.ok():
+            rclpy.spin_once(interface, timeout_sec=0)
+        interface.destroy_node()
+        rclpy.shutdown()
+    except KeyboardInterrupt:
+        pass
 
 if __name__ == "__main__":
     main()
