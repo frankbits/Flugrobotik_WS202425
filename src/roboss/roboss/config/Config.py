720673ee055a239f8b3ead5197214abbe837c9e9
moeux <68925026+moeux@users.noreply.github.com>
2025-02-09 23:05:28 +0100

    Tweaked some drone and pathing parameters and added print statements.

--- src/roboss/roboss/config/Config.py
+++ src/roboss/roboss/config/Config.py
@@ -5,14 +5,14 @@ class Arena:
     SEGMENTS_KEY = "segments"
     OBSTACLE_KEY = "obstacle"
     SEGMENT_SIZE = 0.2
-    SIZE = 1.45
+    SIZE = 1.4
 
 
 class Drone:
     A_TOL = 0.3
     WIDTH = 0.115
     # Defines the swaths width, should be the diameter of the Range Finder sensor spread
-    OPERATIONAL_WIDTH = WIDTH * 2
+    OPERATIONAL_WIDTH = WIDTH
     MIN_TURNING_RADIUS = 0.05
     MAX_DIFF_CURV = 0.25
     INITIAL_X = 0.0
--- src/roboss/roboss/route/F2CRoute.py
+++ src/roboss/roboss/route/F2CRoute.py
@@ -1,15 +1,26 @@
 import math
-
+from typing import List
 import fields2cover as f2c
 
 from ..config import Config
 
 
 class F2CRoute:
-    def test():
+    def getRoute(obstacles: List[List[float]]):
+        """
+        Calculates and returns the path that also avoids the specified obstacles.
+        The path is optimized for field coverage and is calculated by bruteforcing the best swaths and curves are optimized using Dubin's curves.
+
+        :param index: List containing the top left coordinates of obstacles
+        :return: Path
+        """
         robot = f2c.Robot(Config.Drone.WIDTH, Config.Drone.OPERATIONAL_WIDTH)
         robot.setMinTurningRadius(Config.Drone.MIN_TURNING_RADIUS)
         robot.setMaxDiffCurv(Config.Drone.MIN_TURNING_RADIUS)
+
+        print(
+            f"Setup robot ({robot.getWidth()}, {robot.getCovWidth()}) ({robot.getMinTurningRadius()}, {robot.getMaxDiffCurv()})"
+        )
 
         field = [
             (-Config.Arena.SIZE, -Config.Arena.SIZE),
@@ -18,41 +29,53 @@ class F2CRoute:
             (-Config.Arena.SIZE, Config.Arena.SIZE),
             (-Config.Arena.SIZE, Config.Arena.SIZE),
         ]
-        obstacle1 = [(1.0, 1.0), (1.2, 1.0), (1.2, 1.2), (1.0, 1.2), (1.0, 1.0)]
-        obstacle2 = [
-            (-1.0, -1.0),
-            (-1.2, -1.0),
-            (-1.2, -1.2),
-            (-1.0, -1.2),
-            (-1.0, -1.0),
-        ]
-        obstacle3 = [
-            (-0.75, 0.25),
-            (-0.55, 0.25),
-            (-0.55, 0.45),
-            (-0.75, 0.45),
-            (-0.75, 0.25),
-        ]
+
+        print(f"Setup flight area ({Config.Arena.SIZE}x{Config.Arena.SIZE})")
 
         cells = f2c.Cells(f2c.Cell(_createLinearRing(field)))
-        cells.addRing(0, _createLinearRing(obstacle1))
-        cells.addRing(0, _createLinearRing(obstacle2))
-        cells.addRing(0, _createLinearRing(obstacle3))
+        obstacle_rings = []
+
+        for obstacle in obstacles:
+            obstacle_rings.append(
+                [
+                    (obstacle[0], obstacle[1]),
+                    (obstacle[0] + Config.Arena.SEGMENT_SIZE, obstacle[1]),
+                    (
+                        obstacle[0] + Config.Arena.SEGMENT_SIZE,
+                        obstacle[1] - Config.Arena.SEGMENT_SIZE,
+                    ),
+                    (obstacle[0], obstacle[1] - Config.Arena.SEGMENT_SIZE),
+                    (obstacle[0], obstacle[1]),
+                ]
+            )
+
+        for ring in obstacle_rings:
+            cells.addRing(0, _createLinearRing(ring))
+
+        print(
+            f"Added {len(obstacle_rings)} obstacles to be considered during path compuation"
+        )
 
         # decomposer = f2c.DECOMP_TrapezoidalDecomp()
         # decomposer.setSplitAngle(0.5 * math.pi)
         # cells = decomposer.decompose(cells)
 
         headland_generator = f2c.HG_Const_gen()
-        midland = headland_generator.generateHeadlands(cells, 0.1)
+        midland = headland_generator.generateHeadlands(cells, robot.getWidth() / 2.0)
         # cells = decomposer.decompose(midland)
-        mainland = headland_generator.generateHeadlands(cells, 0.2)
+        mainland = headland_generator.generateHeadlands(cells, 2 * robot.getWidth() / 2.0)
+
+        print(f"Generated headlands using {type(headland_generator)}")
 
         objective_function = f2c.OBJ_FieldCoverage()
         swath_generator = f2c.SG_BruteForce()
 
         swaths = swath_generator.generateBestSwaths(
             objective_function, robot.getCovWidth(), mainland
+        )
+
+        print(
+            f"Generated swaths using {type(objective_function)} and {type(swath_generator)}"
         )
 
         # swath_sorter = f2c.RP_Spiral(6)
@@ -64,17 +87,27 @@ class F2CRoute:
         )
         route = route_planner.genRoute(midland, swaths)
 
+        print(
+            f"Generated route using planner: {type(route_planner)} and start point: ({route.startPoint().X()}, {route.startPoint().Y()}) and end point: ({route.endPoint().X()}, {route.endPoint().Y()})"
+        )
+
         turning_base = f2c.PP_DubinsCurves()
         path_planner = f2c.PP_PathPlanning()
         path = path_planner.planPath(robot, route, turning_base)
+
+        print(
+            f"Planned path using planner: {type(path_planner)} and turning base: {type(turning_base)}"
+        )
 
         f2c.Visualizer.figure()
         f2c.Visualizer.plot(cells)
         f2c.Visualizer.plot(mainland)
         f2c.Visualizer.plot(path)
         f2c.Visualizer.save(
-            "/home/rosrunner/Documents/Flugrobotik/TeamRoboss/Flugrobotik_WS202425/path_test.png"
+            "/home/rosrunner/Documents/Flugrobotik/TeamRoboss/Flugrobotik_WS202425/path.png"
         )
+        
+        print("Saved visualization")
 
         return path
 
