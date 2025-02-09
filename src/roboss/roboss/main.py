195365141514fc45b3c2b1d432f615d43277630b
moeux <68925026+moeux@users.noreply.github.com>
2025-02-09 20:42:32 +0100

    Added test path traversal.

--- src/roboss/roboss/main.py
+++ src/roboss/roboss/main.py
@@ -4,35 +4,42 @@ from .config import Config
 from .controller.DroneController import DroneController
 from .controller.ROSDroneInterface import ROSDroneInterface
 from .render.Renderer import draw_board
-from .route.RoutePlanner import RoutePlanner
+from .route.F2CRoute import F2CRoute
+
 
 def main():
     print("Running")
 
     arena = Arena(Config.Arena.PATH)
     draw_board(arena.get_mapped_arena())
-    
+
+    print("Calculating path")
+
+    path = F2CRoute.test()
+
     rclpy.init()
-    
+
     interface = ROSDroneInterface()
     drone = DroneController(interface)
 
     print("Takeoff")
-    drone.controller.sleep(4)
+
     drone.controller.takeoff()
     drone.controller.sleep(4)
-    print("Sending target (1,1,1)")
-    drone.move_to([1.0, 1.0, 1.0])
-    drone.controller.sleep(2)
-    print("Getting range and position")
-    print(drone.controller.get_range(), drone.controller.get_position())
-    print("Sending target (2,2,2)")
-    drone.move_to([2.0, 2.0, 2.0])
-    drone.controller.sleep(2)
-    print("Landing")
+
+    print("Flying path")
+
+    for state in path.getStates():
+        drone.move_to([state.point.X(), state.point.Y(), 1.0])
+
+    print("Resetting and landing")
+
+    drone.move_to([0.0, 0.0, 0.0])
     drone.controller.land()
     drone.controller.sleep(4)
-    
+
+    print("Finished")
+
     try:
         while rclpy.ok():
             rclpy.spin_once(interface, timeout_sec=0)
@@ -40,6 +47,7 @@ def main():
         rclpy.shutdown()
     except KeyboardInterrupt:
         pass
+
 
 if __name__ == "__main__":
     main()
